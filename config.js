e1c860005a375018c4e0870cf4878ecab372f49d
dewshan1 <126068347+dewshan1@users.noreply.github.com>
2023-03-31 15:49:40 +0530

    Update config.js

--- config.js
+++ config.js
@@ -53,10 +53,10 @@ global.ppulsa = '085736178354' //Nomor SimCard Yang Kamu Pake
 global.psaweria = 'https://saweria.com/XiaoYan021' //Link Saweriamu Kalo Nggada Ketik - aja
 
 /*============== NOMOR ==============*/
-global.nomorbot = '68673045381' //Nomor Bot
-global.nomorown = '923470027813' //Nomor Owner
+global.nomorbot = '94786336361' //Nomor Bot
+global.nomorown = '94786336361' //Nomor Owner
 global.namebot = 'Ikratos-MD-v1' //Nama Bot
-global.nameown = 'ᴀʜᴍᴀᴅ ᴀʟɪ🇵🇰' // Nama Owner
+global.nameown =  'IVER' // Nama Owner
 
 
 /*============== STAFF ==============*/
